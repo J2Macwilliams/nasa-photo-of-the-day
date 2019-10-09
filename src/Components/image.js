2a2bb0ad4b16a324ed364ca8eebc6cce1a9690f8
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-09 16:54:51 -0700

    Add path to axios API call. Add styling.

--- src/Components/image.js
+++ src/Components/image.js
@@ -1,32 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Card from "./card";
 
 function Image() {
-  const [image, setImage] =useState([]);
+  const [image, setImage] = useState([]);
 
   useEffect(() => {
     axios
       .get(`https://api.nasa.gov/planetary/apod?api_key=juqFK0XMHkuJYbg0mJM66eF31hMsRNrxhU2Mjhfo`)
       .then(response => {
         console.log(response);
-        // setImage(response.data)
+        setImage(response.data)
       });
   }, []);
 
   return (
     <div className="photoContainer">
       <div className="photo">
-        {image.map((photo) => {
-            return (
-            <Card 
-            title={photo.title}
-            Url={photo.url}
-            explanation={photo.explanation}
-            date={photo.date}
-            />
-            );
-        })}
+          <h2>{image.title}</h2>
+         <img className="APOD" src={image.url} alt="Nasa APOD"/>
+         {/* <p>{image.explanation}</p> */}
+         <p>{image.date}</p>
       </div>
     </div>
   );
